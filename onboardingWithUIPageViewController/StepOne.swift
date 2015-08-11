e36de68f010aeb0386d8ed9192ae3c802dbb8693
Robert Chen <robert.chen@thorntechnologies.com>
2015-08-11 19:23:54 -0400

    Dropping in view controllers files and setting them on the storyboard

---
+++ onboardingWithUIPageViewController/StepOne.swift
@@ -0,0 +1,13 @@
+//
+//  StepOne.swift
+//  onboardingWithUIPageViewController
+//
+//  Created by Robert Chen on 8/11/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class StepOne : UIViewController {
+    
+}
---
+++ onboardingWithUIPageViewController/StepZero.swift
@@ -0,0 +1,13 @@
+//
+//  StepZero.swift
+//  onboardingWithUIPageViewController
+//
+//  Created by Robert Chen on 8/11/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class StepZero : UIViewController {
+    
+}
