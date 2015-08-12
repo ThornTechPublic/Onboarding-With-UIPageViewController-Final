2698b6d93323e7475508033e3f64f83bdc3548c8
Robert Chen <robert.chen@thorntechnologies.com>
2015-08-11 20:56:14 -0400

    Why is the whole thing moving??

--- onboardingWithUIPageViewController/StepOne.swift
+++ onboardingWithUIPageViewController/StepOne.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class StepOne : UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var horizontalConstraint: NSLayoutConstraint!
+    @IBOutlet weak var verticalConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        
+        horizontalConstraint.constant = 0
+        verticalConstraint.constant = 20
+
+        UIView.animateWithDuration(2.0,
+            delay: 0.5,
+            options: .CurveEaseIn,
+            animations: {
+                self.view.layoutIfNeeded()
+            },
+            completion: nil
+        )
+        
+    }
 }
